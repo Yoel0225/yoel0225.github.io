a6a8bffc9969ffd2b0a5e845a4a39905a30d670f
Yoel0225 <150107962+Yoel0225@users.noreply.github.com>
2023-12-16 10:25:59 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function cerrarModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+const deadline = new Date("December 24, 2023 00:00:00").getTime();
+
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = deadline - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s `;
+
+    if (distance <= 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "¡Feliz Navidad, Tía! 🎄";
+        const messageElement = document.getElementById("message");
+        if (messageElement.style.display !== "block") {
+            messageElement.style.display = "block";
+            document.getElementById("abrazoButton").addEventListener("click", function() {
+                document.getElementById("myModal").style.display = "block";
+            });
+        }
+    }
+}, 1000);
